795591bc131103695b7c5711ec443d6bdbf577b5
Tobias <tobias.vanstokkum@cloudnation.nl>
2024-04-08 12:58:48 +0200

    Format code with Terraform fmt in locals / main

--- examples/complete/locals.tf
+++ examples/complete/locals.tf
@@ -97,10 +97,10 @@ locals {
 locals {
   virtual_network_links = {
     link1 = {
-      virtual_network_id   = module.network1.vnet.id
+      virtual_network_id = module.network1.vnet.id
     }
     link2 = {
-      virtual_network_id   = module.network2.vnet.id
+      virtual_network_id = module.network2.vnet.id
     }
   }
 }
